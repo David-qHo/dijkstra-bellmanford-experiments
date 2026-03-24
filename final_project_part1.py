69218b76d27430c4c200fa11f2591993a097cdf4
David-qHo <dah2217@gmail.com>
2026-03-23 20:52:51 -0400

    Finished experiment1() and experiment2()

--- final_project_part1.py
+++ final_project_part1.py
@@ -1,5 +1,9 @@
 import min_heap
 import random
+import matplotlib.pyplot as plt
+import math 
+from itertools import permutations 
+
 
 class DirectedWeightedGraph:
 
@@ -149,18 +153,19 @@ def dijkstra_approx(G, source,k):
         current_node = current_element.value     # Get node 
         dist[current_node] = current_element.key # Get cost 
         for neighbour in G.adj[current_node]:    # For all of the nodes neighbours 
+            if num_relax[neighbour] > 0: # There is still available room to relax this node 
 
             #  G.w(n1, n2) returns the weight of edge from n1 -> n2
             # If this new path is less than the current path (dist[neighbour]) 
             #   - replace it 
             # This is the relaxation 
-            if dist[current_node] + G.w(current_node, neighbour) < dist[neighbour]:
-                if num_relax[neighbour] > 0: # There is still available room to relax this node 
-                    num_relax[neighbour] -= 1 
-                    # Update neighbour node with new shortest distance 
-                    Q.decrease_key(neighbour, dist[current_node] + G.w(current_node, neighbour))
-                    dist[neighbour] = dist[current_node] + G.w(current_node, neighbour)
-                    pred[neighbour] = current_node
+                if dist[current_node] + G.w(current_node, neighbour) < dist[neighbour]:
+                    # if num_relax[neighbour] > 0: # There is still available room to relax this node 
+                        num_relax[neighbour] -= 1 
+                        # Update neighbour node with new shortest distance 
+                        Q.decrease_key(neighbour, dist[current_node] + G.w(current_node, neighbour))
+                        dist[neighbour] = dist[current_node] + G.w(current_node, neighbour)
+                        pred[neighbour] = current_node
     return dist
 
 # Relax each node at most k times
@@ -181,14 +186,50 @@ def bellman_ford_approx(G, source,k):
     for _ in range(G.number_of_nodes()):
         for node in nodes:
             for neighbour in G.adj[node]:
-
-                if dist[neighbour] > dist[node] + G.w(node, neighbour):
-                    if num_relax[neighbour] > 0:  # Still available room to relax this node
-                        num_relax[neighbour] -= 1
-                        dist[neighbour] = dist[node] + G.w(node, neighbour)
-                        pred[neighbour] = node
+                if num_relax[neighbour] > 0: # Still available to relax this node
+                    if dist[neighbour] > dist[node] + G.w(node, neighbour):
+                        # if num_relax[neighbour] > 0:  # Still available room to relax this node
+                            num_relax[neighbour] -= 1
+                            dist[neighbour] = dist[node] + G.w(node, neighbour)
+                            pred[neighbour] = node
     return dist
 
+# Return a graph with n nodes and m edges with upper limit weight upper
+# Limited to i CHOOSE 2 edges; since unique edges
+def create_random_graph(n,m,upper): 
+    G = DirectedWeightedGraph()
+
+    # More edges than possible connections in graph
+    # Return empty graph
+    if m > (n * (n - 1)):
+        return G
+    
+    for i in range(n):
+        G.add_node(i)
+
+
+    # Calculate all possible pairs of nodes, ignore duplicates (u,v) , (v,u) 
+    #  Will only include one of those pairs
+    # Also ignores self loops 
+    subset = list(permutations([k for k in range(n)],2)) 
+                                    
+    s = set() # Store pairs of edges (Used to avoid duplicates)
+
+    k = 0 
+
+
+    # Choose j random edges
+    while k < m: 
+        choice = random.choice(subset) # Choose random pair of edges
+
+        # Check if choice in s
+        if choice not in s: 
+            s.add(choice)
+            G.add_edge(choice[0],choice[1],random.randint(1,upper))
+            k += 1
+    
+    return G
+    
 
 # g = DirectedWeightedGraph()
 
@@ -200,7 +241,10 @@ def bellman_ford_approx(G, source,k):
 # g.add_edge(0,2,100)
 # g.add_edge(1,2,1)
 
-# print(dijkstra_approx(g,0,30000))
+# print(dijkstra(g,1))
+# print(bellman_ford(g,1))
+
+
 
 
 
@@ -215,35 +259,148 @@ def bellman_ford_approx(G, source,k):
 # To fix edges use complete random graph function
 def experiment1(): 
 
-    # 1000 graphs, sizes from 1 to 1000 
+    max_nodes = 150  # Max number of nodes
+    num_graphs = 5   # Number of graphs per # of nodes
 
-    max_nodes = 100 
-    num_graphs = 5
-
+ 
 
     graphs = {}
-    for i in range(1,max_nodes+1): 
+    x = [] 
+    for i in range(1,max_nodes+1,5): 
+        x.append(i)
         graphs[i] = [] # initialize empty graphs 
         for _ in range(num_graphs): # Make 5 graphs for every number of nodes
-            graphs[i].append(create_random_complete_graph(i,2 * i))
+            # Put max-weight as 2 * # of nodes, avoid duplicates
+            graphs[i].append(create_random_complete_graph(i,2 * i)) 
 
     # Compute average total_dist of all 5 graphs
         
         
-    total_dists_dijkstra = []
-    total_dists_bellman= []
+    td_dij_app = []  # Store approximation of dijkstra
+    td_bel_app = []   # Store approximation of bellman
+
+    td_short = []       # Store actual cost of shortest path
+    # td_bel = []       # Store actual cost of shortest path
+
+    # key represents number of nodes
+    for key in graphs.keys(): # For each number of nodes
+        # num_nodes = graphs[key][0].number_of_nodes() # Get number of nodes for this graph
+
+        # Initialize sums for this graph size
+        dij_appS = 0 
+        bel_appS = 0 
+        shortestS = 0 
+        # belS = 0
+
+        for graph in graphs[key]: # For each graph in this 
+            # Compute approximation 
+            dij_appS += total_dist(dijkstra_approx(graph,0, 10)) 
+            bel_appS += total_dist(bellman_ford_approx(graph,0, 10))
+
+            # Compute actual 
+            shortestS += total_dist(dijkstra(graph,0))
+            # belS += total_dist(bellman_ford(graph,0))
+
+        # Divide by num_graphs to compute average
+        td_dij_app.append(dij_appS/num_graphs)
+        td_bel_app.append(bel_appS/num_graphs)
+        td_short.append(shortestS/num_graphs)
+        # td_bel.append(belS/num_graphs)
+
+    
+    plt.plot(x,td_dij_app, label = "Dijkstra Approx")
+    plt.plot(x,td_bel_app, label = "Bellman Approx")
+    plt.plot(x,td_short, label = "Actual")
+    # plt.plot(x,td_bel, label = "Bellman-Ford")
 
 
-    # Put max weight as twice number of nodes, avoid duplicates
-    for i in range(1,max_nodes,10): 
-        graphs.append(create_random_complete_graph(i,2 * i))
+    plt.xlabel("Number of nodes")
+    plt.ylabel("total distance")
+    plt.title("Shortest path algorithms and their approximations (k = 10)")
+    plt.legend()
+
+    plt.show()
+        
+    print(td_dij_app) 
+    print(td_bel_app)
 
 
-    for graph in graphs: 
-        total_dists_dijkstra.append(total_dist(dijkstra_approx(graph,0,)))
+
+# Vary number of edges and see how that impacts approximations 
+def experiment2(): 
+    nodes = 50      # Number of nodes for every graph 
+    max_edges = nodes * (nodes - 1) # Max number of edges (n Choose 2)
+    num_graphs = 5   # Number of graphs per # of edges
+
+ 
+
+    graphs = {} 
+    x = [] 
+
+  
+
+    for i in range(1,max_edges,100): 
+        # print(f"Generating {i}")
+        x.append(i)
+        graphs[i] = [] # initialize empty graphs 
+        for _ in range(num_graphs): # Make 5 graphs for every number of nodes
+            # Put max-weight as 2 * # of nodes, avoid duplicates
+            graphs[i].append(create_random_graph(nodes,i, 2 * nodes)) 
 
 
-    print(graphs) 
+    # Compute average total_dist of all 5 graphs
+        
+        
+    td_dij_app = []  # Store approximation of dijkstra
+    td_bel_app = []   # Store approximation of bellman
+
+    td_short = []       # Store actual cost of shortest path
+    # td_bel = []       # Store actual cost of shortest path
+
+    print(graphs.keys())
+    # key represents number of edges
+    for key in graphs.keys(): # For each number of nodes
+        # print(f"Doing edge: {key}")
+        
+
+        # Initialize sums for this graph size
+        dij_appS = 0 
+        bel_appS = 0 
+        shortestS = 0 
+        # belS = 0
+
+        for graph in graphs[key]: # For each graph in this 
+            # Compute approximation 
+            dij_appS += total_dist(dijkstra_approx(graph,0, 10)) 
+            bel_appS += total_dist(bellman_ford_approx(graph,0, 10))
+
+            # Compute actual 
+            shortestS += total_dist(dijkstra(graph,0))
+
+        # Divide by num_graphs to compute average
+        td_dij_app.append(dij_appS/num_graphs)
+        td_bel_app.append(bel_appS/num_graphs)
+        td_short.append(shortestS/num_graphs)
+
+    
+    plt.plot(x,td_dij_app, label = "Dijkstra Approx")
+    plt.plot(x,td_bel_app, label = "Bellman Approx")
+    plt.plot(x,td_short, label = "Actual")
+    # plt.plot(x,td_bel, label = "Bellman-Ford")
 
 
-experiment1()
+    plt.xlabel("Number of edges")
+    plt.ylabel("total distance")
+    plt.title("Shortest path algorithms and their approximations (k = 10)")
+    plt.legend()
+
+    plt.show()
+        
+    print(td_dij_app) 
+    print(td_bel_app)
+
+
+
+
+
+experiment2()

5c9f04f7c7122ee8352c3e8b34b1300d4335a990
David-qHo <dah2217@gmail.com>
2026-04-01 12:50:49 -0400

    heuristic graph implementation

--- part3_4.py
+++ part3_4.py
@@ -1,6 +1,5 @@
 import math 
 
-
 #Undirected graph using an adjacency list
 class Graph:
     def __init__(self, n):
@@ -31,7 +30,7 @@ class WeightedGraph(Graph):
     def __init__(self):
         self.adj = {}
         self.weights = {} 
-        self.values = {} # Store tuples of longitude and latitude 
+        self.values = {} # Store tuples of (latitude,longitude) 
 
     # Get weight between node1 and node2
     def w(self, node1, node2):
@@ -51,20 +50,21 @@ class WeightedGraph(Graph):
             self.adj[node2].append(node1)
     
         self.weights[(node1, node2)] = weight
+        self.weights[(node2, node1)] = weight
 
 
     
 
 
-wg = WeightedGraph() 
-wg.add_node(1,(3,4))
-wg.add_node(2,(4,5))
+# wg = WeightedGraph() 
+# wg.add_node(1,(3,4))
+# wg.add_node(2,(4,5))
 
-wg.add_edge(1,2,100) 
-print(wg.number_of_nodes())
+# wg.add_edge(1,2,100) 
+# print(wg.number_of_nodes())
 
-print(wg.adj)
-print(wg.values)
+# print(wg.adj)
+# print(wg.values)
 
 
 
@@ -72,22 +72,36 @@ print(wg.values)
 # which takes in another station and returns the straight line distance
 #  from this station to the source station 
 # Compute heuristic from every node to that destination (target)
-class HeuristicGraph: 
+class HeuristicGraph(WeightedGraph): 
     
-    def __init__(self, target): 
-        self.target = target  # Initialize source for this heuristic
+    # Takes a weighted graph and target as argument
+    def __init__(self,n): 
+        super().__init__()
         self.heuristic = {}   # Initialize dictionary
 
 
-    def get_heuristic(): 
-        return  
+        for i in range(1,n+1): 
+            self.heuristic[i] = float("inf")
 
+
+    def get_heuristic(self): 
+        return self.heuristic
     
 
-    def compute_dist(target): 
-        return 
-    
+    def compute_heuristic(self,target): 
+        #calculates the euclidian distance from each node to the target node and returns a hueristic function
+        for node in list(self.adj.keys()):
+            self.heuristic[node] = math.sqrt((self.values[target][0] - G.values[node][0])**2 + (self.values[target][1] - self.values[node][1])**2)
+
+        
 
 
-def compute_heurstic(G,target): 
-    return 
+# hg = HeuristicGraph(compute_heuristic(G,target))
+
+# Own the graph and the algorithm
+# So create the specific object you need inside here 
+class ShortPathFinder: 
+
+    pass 
+
+
